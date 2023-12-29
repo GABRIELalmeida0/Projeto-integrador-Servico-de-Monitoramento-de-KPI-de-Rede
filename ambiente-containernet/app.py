1e3ff3c589e1d550525ec454ff056894b5a87438
jcfenuchi <juliocesarfenuchi@hotmail.com>
2023-12-29 01:17:48 -0300

    atualização no app.py

--- ambiente-containernet/app.py
+++ ambiente-containernet/app.py
@@ -18,20 +18,22 @@ class App():
             l = "0%" if request.args.get("loss") is None else request.args.get("loss")
             if node:
                 if node in [node.name for node in self.net.hosts]:
-                    node = self.net.getNodeByName(node)
                     # Relacionado as mudanças dos parametros da rede
                     print(f"alterando parametros da rede para {d} {j} {l} ...")
-                    node.cmd(f"python3 alteracao_rede.py {node.name}-eth0 {d} {j} {l}")
+                    switch = self.net.getNodeByName("s1")
+                    switch.cmd(f"python3 alteracao_rede.py {switch.name}-eth2 {d} {j} {l}")
 
+                    node = self.net.getNodeByName(node)
                     # Relacionado com a captura streaming
-                    print("Criando Experimento ...")
                     video_name = f'{self.default_url.split("/")[-1]}.mp4'
                     experiment_num = len([dir for dir in listdir("videos") if dir.startswith("experiment")])+1
+                    print(f"Criando Experimento-{experiment_num} ...")
                     mkdir(f"videos/experiment-{experiment_num}")
                     print("iniciando a captura do video ...")
                     cmd = f"ffmpeg -i {self.default_url} -c copy -preset ultrafast -r 60 -b 900k /save-videos/experiment-{experiment_num}/{video_name}"
                     capture_video_process = node.popen(cmd)
                     capture_video_process.wait()
+
                     # Retirada das Métricas de SSIM do video capturado e do recebido
                     print("iniciando a comparação dos videos ...")
                     node.cmd(f'echo "delay-{d}, jitter-{j}, loss-{l}" > /save-videos/experiment-{experiment_num}/metrics.txt')
--- ambiente-containernet/curl.sh
+++ ambiente-containernet/curl.sh
@@ -1,3 +1,3 @@
-curl "http://localhost:5000/capture_streaming?node_name=d2&delay=10ms&jitter=20ms&loss=5%"
-curl "http://localhost:5000/capture_streaming?node_name=d2&delay=10ms&jitter=5ms&loss=0.1%"
-curl "http://localhost:5000/capture_streaming?node_name=d2&delay=50ms&jitter=50ms&loss=2%"
+curl "http://localhost:5000/capture_streaming?node_name=d2&delay=20ms&jitter=50ms&loss=5%"
+curl "http://localhost:5000/capture_streaming?node_name=d2&delay=25ms&jitter=50ms&loss=0.1%"
+curl "http://localhost:5000/capture_streaming?node_name=d2&delay=200ms&jitter=200ms&loss=2%"
